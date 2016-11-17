72a7f4ec4ae04abef67d785dcd4418dd218a3f3a
Razvan Sdira <sdira_razvan@yahoo.com>
2016-11-17 10:42:03 +0200

    Add files via upload

---
+++ Exercise1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_For_Loop
+{
+    class Exercise1
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            Console.WriteLine(" The first ten natural numbers: ");
+
+            for (x = 1; x <= 10; x++)
+                Console.Write(" {0}", x);
+            Console.ReadLine();
+        }
+    }
+}
