32474037400b83f5d79e52cd11f1467dd5b047ec
Razvan Sdira <sdira_razvan@yahoo.com>
2016-11-20 13:28:32 +0200

    Add files via upload

---
+++ Exercise6.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_For_Loop
+{
+    class Exercise6
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.Write("Input a number: ");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            for (b = 0; b <= 10; b++)
+            {
+                Console.WriteLine("{0} x {1} = {2} ", a, b, a * b);
+            }
+            Console.ReadLine();
+        }
+    }
+}
