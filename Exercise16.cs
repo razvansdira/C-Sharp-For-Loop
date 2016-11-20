aaea6a467c37d8f9d2f021402b605ba4439cb71e
Razvan Sdira <sdira_razvan@yahoo.com>
2016-11-20 21:56:50 +0200

    Add files via upload

---
+++ Exercise16.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_For_Loop
+{
+    class Exercise16
+    {
+        static void Main(string[] args)
+        {
+            int x, y, z=0;
+
+            Console.Write(" Input a number: ");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.Write("The even numbers are: ");
+
+            for (y=1; y<=x; y++)
+            {
+                Console.Write("{0 } ", 2 * y);
+                z += 2*y;
+            }
+            Console.Write("\n The sum is: {0}", z);
+            Console.ReadLine();
+        }
+    }
+}
