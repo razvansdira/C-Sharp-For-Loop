f91391d8c397a7da2b37ce1e25bd804636cdc5a6
Razvan Sdira <sdira_razvan@yahoo.com>
2016-11-20 15:10:15 +0200

    Add files via upload

---
+++ Exercise9.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_For_Loop
+{
+    class Exercise9
+    {
+        static void Main(string[] args)
+        {
+            int i, j, k;
+            Console.Write("Input a number: ");
+            k = Convert.ToInt32(Console.ReadLine());
+
+            for (i = 1; i <= k; i++)
+            {
+                for (j = 1; j <= i; j++)
+                    Console.Write("* ");
+                Console.Write("\n");
+            }               
+            Console.ReadLine();
+        }
+    }
+}
