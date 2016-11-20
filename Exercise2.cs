09bca40e1fb63b6e781c9ee5db3bc6cb56807a48
Razvan Sdira <sdira_razvan@yahoo.com>
2016-11-20 11:22:59 +0200

    Add files via upload

---
+++ Exercise2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_For_Loop
+{
+    class Exercise2
+    {
+        static void Main (String[] args)
+        {
+            int x, y=0;
+            Console.WriteLine(" The sum of first ten natural numbers: ");
+
+            for (x = 1; x <= 10; x++)
+                y = y + x;
+                Console.Write(" {0}", y);
+            Console.ReadLine();
+        }
+    }
+}
