e084f3e8c96f7fdbc2ff38881024781bc3a6244a
Razvan Sdira <sdira_razvan@yahoo.com>
2016-11-20 21:45:55 +0200

    Add files via upload

---
+++ Exercise15.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_For_Loop
+{
+    class Exercise15
+    {
+        static void Main(string[] args)
+        {
+            int x, y, z=1;
+
+            Console.Write("Input a number: ");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            for (y = 1; y <= x; y++)
+                z = z * y;
+            Console.Write("The factorial of {0} is equal with {1}.", x, z);
+
+            Console.ReadLine();
+        }
+    }
+}
