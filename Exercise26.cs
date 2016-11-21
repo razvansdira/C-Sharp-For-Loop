dd4007350dadfd1e25901ce8ff7e1e66064bf19a
Razvan Sdira <sdira_razvan@yahoo.com>
2016-11-21 12:18:58 +0200

    Add files via upload

---
+++ Exercise26.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_For_Loop
+{
+    class Exercise26
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c = 0, d = 1;
+            Console.Write(" Input the number of terms: ");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            for(b=1; b<=a; b++)
+            {
+                c += d;
+                Console.Write("{0} ", d);
+                d = d * 10 + 1;
+            }
+            Console.Write("\n The sum is: {0}",c);
+            Console.ReadLine();
+        }
+    }
+}
