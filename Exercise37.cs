89a1a7916b82eb1261b29686d909f387fdf951cb
Razvan Sdira <sdira_razvan@yahoo.com>
2016-11-22 13:50:56 +0200

    Add files via upload

---
+++ Exercise37.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_For_Loop
+{
+    class Exercise37
+    {
+        static void Main(string[] args)
+        {
+            int x, y, z, sum=0;
+            Console.Write(" Input a series of number: ");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            for(y=x; y!=0; y=y/10)
+            {
+                z = y % 10;
+                sum = sum * 10 + z;
+            }
+            Console.Write("The reverse order is: {0}", sum);
+            Console.ReadLine();
+        }
+    }
+}
