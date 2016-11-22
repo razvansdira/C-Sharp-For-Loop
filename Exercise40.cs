50d7ec74027577bc7e6b53fe09cb3af63b3343ef
Razvan Sdira <sdira_razvan@yahoo.com>
2016-11-22 19:40:21 +0200

    Add files via upload

---
+++ Exercise40.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_For_Loop
+{
+    class Exercise40
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, d=1;
+            char abc = 'A';
+
+            Console.Write("  Input a number: ");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            for (c = 1; c <= a; c++)
+            {
+                for (b = 0; b <= (d / 2); b++)
+                {
+                    Console.Write("{0}", abc++);
+                }
+                abc--; abc--;
+
+                for (b = 0; b < (d / 2); b++)
+                {
+                    Console.Write("{0}", abc--);
+                }
+                d += 2;
+                abc = 'A';
+                Console.Write("\n");
+            }
+            Console.ReadLine();
+        }
+    }
+}
