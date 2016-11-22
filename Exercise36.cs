0de04f6211c3236adf706836ee90f794fef0a82d
Razvan Sdira <sdira_razvan@yahoo.com>
2016-11-22 13:34:13 +0200

    Add files via upload

---
+++ Exercise36.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_For_Loop
+{
+    class Exercise36
+    {
+        static void Main (string[] args)
+        {
+            int a, b, c;
+            Console.Write("Input a number of rows: ");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            for (b = 0; b <= a; b++)
+            {
+                for(c=1; c<=a-b; c++)
+                    Console.Write(" ");
+                for (c = 1; c <= b; c++)
+                    Console.Write("{0}", c);
+                for (c = b - 1; c >= 1; c--)
+                    Console.Write("{0}", c);
+                Console.Write("\n");
+            }
+            Console.ReadLine();
+        }
+    }
+}
