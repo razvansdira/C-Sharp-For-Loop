2c49b3db3339dc62fc488f33230dfa544ae7dd03
Razvan Sdira <sdira_razvan@yahoo.com>
2016-11-22 11:12:19 +0200

    Add files via upload

---
+++ Exercise32.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_For_Loop
+{
+    class Exercise32
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c=0;
+            Console.Write("Input a number: ");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            for (b=2; b<=a/2; b++)
+            {
+                if (a % b == 0)
+                    c++;
+            }
+            if (c == 0 && a != 1)
+                Console.Write(" {0} is a prime number.", a);
+            else
+                Console.Write(" {0} isn't a prime number.", a);
+            
+            Console.ReadLine();
+        }
+    }
+}
