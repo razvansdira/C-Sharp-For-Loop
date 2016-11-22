76e79632c021383f2fc0e7eb5d33e92c2fc1585b
Razvan Sdira <sdira_razvan@yahoo.com>
2016-11-22 22:35:45 +0200

    Add files via upload

---
+++ Exercise44.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_For_Loop
+{
+    class Exercise44
+    {
+        static void Main(string[] args)
+        {
+            int m, n, i, j, hcf = 1, lcm;
+
+            Console.Write(" Input first number for LCM: ");
+            m = Convert.ToInt32(Console.ReadLine());
+            Console.Write(" Input second number: ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            j = (m < n) ? m : n;
+            for (i = 1; i <= j; i++)
+            {
+                if (m % i == 0 && n % i == 0)
+                    hcf = i;
+            }
+            lcm = (m * n) / hcf;
+
+            Console.Write("The LCM number is: {0}.", lcm);
+            Console.ReadLine();
+        }
+    }
+}
