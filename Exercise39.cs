fb08faf3ab55d38b6c383a4b3d8331362fa654dd
Razvan Sdira <sdira_razvan@yahoo.com>
2016-11-22 19:24:49 +0200

    Add files via upload

---
+++ Exercise39.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_For_Loop
+{
+    class Exercise39
+    {
+        static void Main(string[] args)
+        {
+            int a, sum = 0;
+
+            Console.WriteLine("  Numbers between 100 and 200, divisible by 9:");
+            for (a = 100; a <= 200; a++)
+            {
+                if (a % 9 == 0)
+                {
+                    Console.Write("{0}  ", a);
+                    sum += a;
+                }
+            }
+            Console.Write("\n  The sum is: {0} ", sum);
+            Console.ReadLine();
+        }
+    }
+}
