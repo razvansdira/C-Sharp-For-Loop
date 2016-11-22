d943af7cefad0e81c7be866ca55346bd9aa84848
Razvan Sdira <sdira_razvan@yahoo.com>
2016-11-22 21:33:28 +0200

    Add files via upload

---
+++ Exercise41.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_For_Loop
+{
+    class Exercise41
+    {
+        static void Main(string[] args)
+        {
+            int x, i=1, j, r, s=0;
+            Console.Write(" Input a number to convert: ");
+            x = Convert.ToInt32(Console.ReadLine());
+            r = x;
+
+            for(j=x; j>0; j /= 2)
+            {
+                s += (x % 2) * i;
+                i *= 10;
+                x /= 2;
+            }
+            Console.Write("The binary is: {0}.", s);
+            Console.ReadLine();
+        }
+    }
+}
