3e62b658103babc7630e18dc03b50bd3f86e50db
Razvan Sdira <sdira_razvan@yahoo.com>
2016-11-22 10:01:21 +0200

    Add files via upload

---
+++ Exercise28.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_For_Loop
+{
+    class Exercise28
+    {
+        static void Main (string[] args)
+        {
+            int x, y, i, j, k;
+
+            Console.Write("Input the starting range of number: ");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input the ending range of number: ");
+            y = Convert.ToInt32(Console.ReadLine());
+
+            for(i=x; i<=y; i++)
+            {
+                j = 1;
+                k = 0;
+                while(j<i)
+                {
+                    if (i % j == 0)
+                        k += j;
+                    j++;
+                }
+                if (k == i)
+                    Console.Write(" {0} ", i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
