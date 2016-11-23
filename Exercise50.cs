02e0d5635e5992abb306dd355bd5f7ff0f2a958a
Razvan Sdira <sdira_razvan@yahoo.com>
2016-11-23 11:50:17 +0200

    Add files via upload

---
+++ Exercise50.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_For_Loop
+{
+    class Exercise50
+    {
+        static void Main(string[] args)
+        {
+            int x, i = 1, j, r, s = 0;
+            Console.Write(" Input a number to convert: ");
+            x = Convert.ToInt32(Console.ReadLine());
+            r = x;
+
+            for (j = x; j > 0; j /= 8)
+            {
+                s += (j % 8) * i;
+                i *= 10;
+                x /= 2;
+            }
+            Console.Write("The octal number is: {0}.", s);
+            Console.ReadLine();
+        }
+    }
+}
