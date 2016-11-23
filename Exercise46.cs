b159ede922492b6134f0d962ec0016a8efca5bfa
Razvan Sdira <sdira_razvan@yahoo.com>
2016-11-23 10:45:38 +0200

    Add files via upload

---
+++ Exercise46.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_For_Loop
+{
+    class Exercise46
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            double s, i = 0, dec = 0;
+            Console.Write(" Input a binary number to convert: ");
+            x = Convert.ToInt32(Console.ReadLine());
+            y = x;
+
+
+            while (x != 0)
+            {
+                s = x % 10;
+                dec += s * Math.Pow(2, i);
+                x /= 10;
+                i++;
+            }
+            Console.Write("The decimal number is: {0}.", dec);
+            Console.ReadLine();
+        }
+    }
+}
