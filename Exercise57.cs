bc2204527407e303009facc04befd622e3db5eae
Razvan Sdira <sdira_razvan@yahoo.com>
2016-11-23 15:47:05 +0200

    Add files via upload

---
+++ Exercise57.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_For_Loop
+{
+    class Exercise57
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            string a, b = "";
+
+            Console.Write(" Input a word: ");
+            a = Console.ReadLine();
+
+            x = a.Length - 1;
+            for (y = x; y >= 0; y--)
+            {
+                b += a[y];
+            }
+            Console.Write("The reverse order is: {0}", b);
+            Console.ReadLine();
+        }
+    }
+}
